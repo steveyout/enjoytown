89e9f8628c8b708fc736e95726b2daf5892aeece
steveyout <youtsteve1@gmail.com>
2025-04-28 16:25:46 +0300

    added scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
         </Script>
         <Script
           type="text/javascript"
-          src="//dandyleapt.com/86170904905e8f88e4232086dfbb050b/invoke.js"
+          src="//direcompulsion.com/86170904905e8f88e4232086dfbb050b/invoke.js"
         />
         <Script type="text/javascript" id="ads1">
           {`
@@ -71,7 +71,7 @@ export default function RootLayout({
         </Script>
         <Script
           type="text/javascript"
-          src="//dandyleapt.com/b62e6028017550fbd2a568f66726246c/invoke.js"
+          src="//direcompulsion.com/b62e6028017550fbd2a568f66726246c/invoke.js"
         />
         <Script async strategy="afterInteractive" src="//acscdn.com/script/aclib.js"></Script>
         <Script data-cfasync="false" strategy="lazyOnload" id="adcash">
